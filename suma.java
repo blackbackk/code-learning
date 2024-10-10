5f425fa1b1a32aeed95aa10e91f234842f1e53f3
blackback <miguelacamara2600@gmail.com>
2024-10-10 12:04:52 +0200

    Suma Java

--- suma.java
+++ suma.java
@@ -1,7 +1,7 @@
 public class suma {
     public static void main(String[] args) {
         int suma = 1+2+3+4+5;  
-        System.out.println("suma");
+        System.out.println("suma"); 
     }
 
 }

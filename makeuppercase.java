4220bbf518eacc4657b580681e6df8ae1a2811d3
blackback <miguelacamara2600@gmail.com>
2024-10-10 12:45:51 +0200

    UpperCase-CW

--- makeuppercase.java
+++ makeuppercase.java
@@ -1,4 +1,4 @@
-class Upper{
+class makeuppercase{
     //Write a function which converts the input string to uppercase.
   public static String MakeUpperCase(String str){
     return str.toUpperCase(); 

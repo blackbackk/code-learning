d0f1421a853d7d7bca366f69a14697985cf66bb7
blackback <miguelacamara2600@gmail.com>
2024-10-11 10:32:53 +0200

    discounts

--- discounts.java
+++ discounts.java
@@ -1,5 +1,24 @@
 import java.util.Scanner;
 public class discounts {
+// Han empezado las rebajas en nuestra tienda de ropa y hace falta ajustar
+// el sistema de cálculo del precio final de cada pieza. ¡Un montón de trabajo!
+// Por suerte podemos hacer un algoritmo simple que nos simplificará
+// el trabajo. Nuestra promoción es la siguiente: a todos los artículos les
+// aplicamos un 25 % de descuento, pero si se compran más de tres piezas,
+// entonces se aplica el 50 % a todas ellas. ¡Una ganga!
+// Entendemos el problema
+// Tenemos dos datos de entrada: el precio de compra y el número de
+// artículos totales de la compra. Y, como salida, nos piden que calculemos
+// el importe tras aplicar las rebajas.
+// Pensamos cómo resolverlo
+// Decidimos los cálculos a realizar para obtener lo que pide el enunciado:
+// •
+// El tipo de descuento es del 25 % si el número de productos es de 1
+// a 3, y del 50 % si es de más de 3.
+// •
+// Importe descontado = importe x tipo de descuento.
+// •
+// Importe final: importe – importe descontado.
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
